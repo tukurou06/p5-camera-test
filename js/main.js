1d3509ba9592d2d65ac219305cf49384502e6994
yoshbo <yoshbo@gmail.com>
2019-03-22 08:00:15 +0900

    1st commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+var canvas;
+var width_main = $(".main").width();
+var cnt = 0;
+
+let capture;
+
+function setup(){
+    //canvas = createCanvas(width_main, 400);
+    //canvas.parent("p5Canvas");
+    background(200);
+    
+    var constraints = {
+        video: {
+            //facingMode: "user"
+            facingMode: "environment"
+//            facingMode: { 
+//               exact: "environment"
+//           }
+        },
+        audio: false
+    };
+
+    createCanvas(640, 480);
+    //capture = createCapture(VIDEO);
+    capture = createCapture(constraints);
+    capture.size(640, 480);
+
+    // 元のビデオは隠す
+    capture.hide();    
+}
+
+function draw(){
+    background(230);
+
+    image(capture, 0, 0, 640, 480);
+
+    fill('#fff');
+    textSize(24);
+    text(cnt,10,30);
+
+    cnt++;
+}
